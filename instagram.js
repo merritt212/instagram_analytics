b0de3dc80a4ccfcb3e5c2846536ed94f82b1f87a
pariskshitdutt <pdutt111>
2015-03-03 10:28:00 +0530

    no globals

--- instagram.js
+++ instagram.js
@@ -15,6 +15,7 @@ var ig = require('instagram-node').instagram();
  * express app made and an event emitter used for managing different events
  */
 var app =express();
+app.use(jsonStream());
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var event = new EventEmitter();
@@ -28,11 +29,6 @@ ig.use({ client_id: '0c6beda06dc446b7957d043957314ce7', client_secret: '73021c83
  * global variables
  */
 var port=5000           //port on which the service will run
-var followers=[];       //store all the ids of the followers
-var posts=[];           //store the timestamp of all the posts
-var max_calls={};       //max_calls used for managing that too many pages are not crawled per follower
-var max_posts=10000;     //to prevent fetching too many posts limit on the number of posts to 5000
-var number=0;           //keep track of the number of follower data crawled
 var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];     //required to sort data according to day string
 
 /**
@@ -45,13 +41,12 @@ var weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Satu
  */
 var followercallback = function(err, result, pagination, remaining, limit) {
     for(var i=0;i<result.length;i++){
-        followers.push(result[i].id);
+        this.followers.push(result[i].id);
     }
-    event.emit('loading followers',followers.length+" followers fetched");
     if(pagination.next) {
-        pagination.next(followercallback); // Will get second page results
+        pagination.next(followercallback.bind({user_id:this.user_id,followers:this.followers,posts:this.posts,res:this.res})); // Will get second page results
     }else{
-        completedfollowers();
+        completedfollowers(this.followers,this.posts,this.user_id,this.res);
     }
 };
 
@@ -66,33 +61,29 @@ var followercallback = function(err, result, pagination, remaining, limit) {
 var postscallback = function(err, medias, pagination, remaining, limit) {
     if(!err) {
         for (var i = 0; i < medias.length; i++) {
-            posts.push(new Date(parseInt(medias[i].created_time) * 1000));
+            this.posts.push(new Date(parseInt(medias[i].created_time) * 1000));
         }
-        event.emit('loading posts',posts.length+" posts fetched");
 
-        if (pagination.next&&max_calls[this.id]<10&&posts.length<max_posts) {
-            if(!max_calls[this.id]){
-                max_calls[this.id]=1;
-            }else{
-                max_calls[this.id]++;
-            }
-            pagination.next(postscallback.bind({id:this.id})); // Will get second page results
+        if (pagination.next) {
+            pagination.next(postscallback.bind({id:this.id,posts:this.posts,number:this.number,user_id:this.user_id,res:this.res})); // Will get second page results
         } else {
             //console.log(this.i);
-            completedposts();
+            if(this.number==this.total)
+            completedposts(this.posts,this.user_id,this.res);
         }
     }else{
         console.log(err);
-        completedposts();
+        if(this.number==this.total)
+            completedposts(this.posts,this.user_id,this.res);
     }
 };
 /**
  * called when the crawling of followers is completed this starts the crawling for posts by the followers
  */
-function completedfollowers(){
-    console.log(followers.length);
+function completedfollowers(followers,posts,user_id,res){
+    console.log("followers completed "+this.user_id,followers.length);
     for(var i=0;i<followers.length;i++) {
-        ig.user_media_recent(followers[i],{count:100}, postscallback.bind({id:followers[i]}));
+        ig.user_media_recent(followers[i],{count:100}, postscallback.bind({id:followers[i],total:(followers.length-1),posts:posts,number:i,user_id:user_id,res:res}));
     }
 
 }
@@ -100,10 +91,7 @@ function completedfollowers(){
 /**
  * called on completion of crawl of the posts
  */
-function completedposts(){
-    number++;
-    console.log(number);
-    if(number==followers.length) {
+function completedposts(posts,user_id,res){
         var post_days={};
         var post_time={};
         for (var i = 0; i < posts.length; i++) {
@@ -141,22 +129,22 @@ function completedposts(){
         }
         console.log(chartdays);
         console.log(charttime);
-        event.emit('done',{days:chartdays,time:charttime,best:bestDay+" at "+bestTime+" hours"});
-    }
+        res.json({days:chartdays,time:charttime,best:bestDay+" at "+bestTime+" hours"});
+        //res.end();
 }
 
 /**
  * call to start the crawling process
  * @param id
  */
-function getDetailsofUser(id){
-    ig.user_followers(id,{count:100},followercallback);
+function getDetailsofUser(id,followers,posts,res){
+    ig.user_followers(id,{count:100},followercallback.bind({user_id:id,followers:followers,posts:posts,res:res}));
 }
+
 function getuserid(name){
-    number=0;
-    posts=[];
-    max_calls={};
-    followers=[];
+    //var number=0;
+    //var posts=[];
+    //var followers=[];
     ig.user_search(name, function(err, users, remaining, limit) {
         var found=false;
             for (var i = 0; i < users.length; i++) {
@@ -179,31 +167,40 @@ app.get('/', function(req, res){
     res.sendFile(__dirname +'/index.htm');
 });
 
+app.get('/instagram/:id',function(req,res){
+    console.log(req.params.id);
+    var followers=[];       //store all the ids of the followers
+    var posts=[];           //store the timestamp of all the posts
+    req.socket.setTimeout(Infinity);
+    getDetailsofUser(req.params.id,followers,posts,res);
+
+})
+
 /**
  * socket.io events to send and recieve data to the client side
  */
 io.on('connection', function(socket){
-    console.log("user connected");
-    event.on('done',function(data){
+   console.log("user connected");
+   event.on('done',function(data){
         io.emit('init',data);
-    });
-    event.on('loading posts',function(data){
+   });
+   event.on('loading posts',function(data){
         io.emit('loading posts',data);
-    });
-    event.on('loading followers',function(data){
+   });
+   event.on('loading followers',function(data){
         io.emit('loading followers',data);
-    });
-    event.on("no such user",function(){
+   });
+   event.on("no such user",function(){
         io.emit("no user");
-    })
-    socket.on('username',function(name){
+   });
+   socket.on('username',function(name){
         console.log(name);
         getuserid(name);
-    });
+   });
    socket.on('instagram id',function(id){
        console.log(id);
        getDetailsofUser(id);
-   })
+   });
 });
 http.listen(port, function(){
     console.log('listening on *:'+port);
